dbf06e5227d87bb637833a3220d93e9f01171db6
Sven Mark <hofs1600@student.nhl.nl>
2018-04-23 13:25:51 +0200

    Cleaner, fewer if's

--- src/main.py
+++ src/main.py
@@ -2,6 +2,19 @@ import sys
 
 from elements import element1, element2, element3, element4, element5, element6, element7, element8, element9, element10
 
+FUNC_MAP = {
+    "1": element1,
+    "2": element2,
+    "3": element3,
+    "4": element4,
+    "5": element5,
+    "6": element6,
+    "7": element7,
+    "8": element8,
+    "9": element9,
+    "10": element10
+}
+
 
 def main():
     # print command line arguments
@@ -11,29 +24,9 @@ def main():
 
     part = sys.argv[1]
 
-    if part == "1":
-        element1.run()
-    elif part == "2":
-        element2.run()
-    elif part == "3":
-        element3.run()
-    elif part == "4":
-        element4.run()
-    elif part == "5":
-        element5.run()
-    elif part == "6":
-        element6.run()
-    elif part == "7":
-        element7.run()
-    elif part == "8":
-        element8.run()
-    elif part == "9":
-        element9.run()
-    elif part == "10":
-        element10.run()
-    else:
-        print("Not a valid element")
-        sys.exit(1)
+    part_function = FUNC_MAP[part]
+
+    part_function.run()
 
 
 if __name__ == "__main__":
