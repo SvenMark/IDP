e6fad4c2fbdf1c475d2caa58ebd7856382397646
AltijdMelvin <bos12bos@hotmail.com>
2018-04-24 15:28:06 +0200

    Play and record werkend.

--- src/entities/audio/listen.py
+++ src/entities/audio/listen.py
@@ -3,25 +3,22 @@ import sounddevice as sd
 
 
 class Listen(Audio):
-    SAMPLE_RATE = 44100
-    DURATION = 5  # secondes
-    CHANNELS = 2  # stereo
+    @staticmethod
+    def record():
+        SAMPLE_RATE = 44100
+        DURATION = 5  # secondes
+        CHANNELS = 2  # stereo
 
-    recording = sd.rec(int(DURATION * SAMPLE_RATE), samplerate=SAMPLE_RATE, channels=CHANNELS)
-    sd.wait()
-    sd.play(recording)
-    sd.wait()
+        recording = sd.rec(int(DURATION * SAMPLE_RATE), samplerate=SAMPLE_RATE, channels=CHANNELS)
+        sd.wait()
+        sd.play(recording)
+        sd.wait()
 
 
+# Just for testing purposes
 def main():
-    SAMPLE_RATE = 44100
-    DURATION = 5  # secondes
-    CHANNELS = 2  # stereo
-
-    recording = sd.rec(int(DURATION * SAMPLE_RATE), samplerate=SAMPLE_RATE, channels=CHANNELS)
-    sd.wait()
-    sd.play(recording)
-    sd.wait()
+    x = Listen()
+    x.record()
 
 
 main()
--- src/entities/audio/speak.py
+++ src/entities/audio/speak.py
@@ -4,15 +4,20 @@ import soundfile as sf
 
 
 class Speak(Audio):
-    data, fs = sf.read('../../../resources/test.wav', dtype="float32")
-    sd.wait()
-    sd.play(data, fs, device=sd.default.device)
-    sd.wait()
+    @staticmethod
+    def play():
+        data, fs = sf.read('../../../resources/test.wav', dtype="float32")
+        sd.wait()
+        sd.play(data, fs, device=sd.default.device)
+        sd.wait()
 
 
+# Just for testing purposes
 def main():
-    data, fs = sf.read('../../../resources/test.wav', dtype="float32")
-    sd.wait()
-    sd.play(data, fs, device=sd.default.device)
-    sd.wait()
+    x = Speak()
+    x.play()
+
+
+main()
+
 
