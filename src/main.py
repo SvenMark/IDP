b9a462eab10a3128b155d63c105420a00ec03bc3
Sven Mark <hofs1600@student.nhl.nl>
2018-04-24 09:22:32 +0200

    Update main.py Better folder structure

--- src/main.py
+++ src/main.py
@@ -1,27 +1,19 @@
 import sys
 
-from elements.elements10 import element10
-from elements.elements9 import element9
-from elements.elements8 import element8
-from elements.elements7 import element7
-from elements.elements6 import element6
-from elements.elements5 import element5
-from elements.elements4 import element4
-from elements.elements3 import element3
-from elements.elements2 import element2
-from elements.elements1 import element1
+from elements import element1, element2, element3, element4, element5, element6, element7, element8, element9, \
+    element10
 
 FUNC_MAP = {
-    "1": element1,
-    "2": element2,
-    "3": element3,
-    "4": element4,
-    "5": element5,
-    "6": element6,
-    "7": element7,
-    "8": element8,
-    "9": element9,
-    "10": element10
+    "1": element1.core,
+    "2": element2.core,
+    "3": element3.core,
+    "4": element4.core,
+    "5": element5.core,
+    "6": element6.core,
+    "7": element7.core,
+    "8": element8.core,
+    "9": element9.core,
+    "10": element10.core
 }
 
 
@@ -35,6 +27,7 @@ def main():
 
     part_function = FUNC_MAP[part]
 
+    # run element
     part_function.run()
 
 
