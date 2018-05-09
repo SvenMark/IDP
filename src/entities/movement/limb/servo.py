31096aa56b06066e0dd02250a102e7092ebe825e
Jildert <jildert.venema@hotmail.com>
2018-05-09 11:49:35 +0200

    servo class

--- src/entities/movement/limb/servo.py
+++ src/entities/movement/limb/servo.py
@@ -1,9 +1,20 @@
 #!/bin/python
 
+from libs.ax12 import Ax12
+import time
+
 class Servo(object):
     """
     Base class for servo
     """
+
+    def __init__(self, servo_id):
+        self.ax12 = Ax12()
+        self.servo_id = servo_id
+
+    def move(self, degrees, delay):
+        self.ax12.move(self.servo_id, degrees)
+        time.sleep(delay)
 
     @property
     def turn(self):
