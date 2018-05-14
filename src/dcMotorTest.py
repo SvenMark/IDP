035c4c80e82931b49f4f6e2e115892783f316970
Rendall Schijven <rendall.schijven@hotmail.com>
2018-05-14 15:04:20 +0200

    updated test

--- src/dcMotorTest.py
+++ src/dcMotorTest.py
@@ -6,17 +6,13 @@ from entities.movement.limb.dcmotor import DCMotor
 motor = DCMotor()
 
 for cycle in range(0, 20):
-    motor.forward(cycle)
-    time.sleep(0.5)
+    motor.forward(cycle, 0.5)
 
 for cycle in range(20, 0):
-    motor.forward(cycle)
-    time.sleep(0.5)
+    motor.forward(cycle, 0.5)
 
 for cycle in range(0, 20):
-    motor.backward(cycle)
-    time.sleep(0.5)
+    motor.backward(cycle, 0.5)
 
 for cycle in range(20, 0):
-    motor.backward(cycle)
-    time.sleep(0.5)
+    motor.backward(cycle, 0.5)
