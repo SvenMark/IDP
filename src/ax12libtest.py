eb3f7240b309438dd324a64bfbf25c0a0b83b4e8
Rendall Schijven <rendall.schijven@hotmail.com>
2018-05-14 11:25:01 +0200

    updated servotest

--- src/ax12libtest.py
+++ src/ax12libtest.py
@@ -6,11 +6,4 @@ boris = Ax12()
 ax12id = 13
 
 while True :
-  boris.move(ax12id,0)
-  time.sleep(5)
-
-  boris.move(ax12id,180)
-  time.sleep(5)
-
-  boris.move(ax12id,-180)
-  time.sleep(5)
+  boris.readPosition(ax12id)
