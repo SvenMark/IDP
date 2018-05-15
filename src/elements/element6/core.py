059a42ff9ba398498985221a9354f1c273dd7668
AltijdMelvin <bos12bos@hotmail.com>
2018-05-15 14:14:00 +0200

    Line detection working

--- src/elements/element6/core.py
+++ src/elements/element6/core.py
@@ -5,47 +5,36 @@ import time
 import numpy as np
 import cv2
 
-print ("uncomment run before starting..")
+
 def run():
     print("run element6")
     cap = WebcamVideoStream(src=0).start()
-    time.sleep(1) #startup
+    time.sleep(1)  # startup
 
     while True:
         img = cap.read()
-        blur = cv2.GaussianBlur(img, (5, 5), 0)
-        blurgray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
+        blur = cv2.GaussianBlur(img, (9, 9), 0)
         hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
 
-        #Cranny edges
-        low_threshold = 100
-        high_threshold = 200
-        edges = cv2.Canny(blurgray, low_threshold, high_threshold)
-        
-        #Mask
-        low_white = np.array([0, 0, 0])   
-        high_white = np.array([180,255,30])
-        mask = cv2.inRange(hsv,low_white,high_white) 
+        # Mask
+        low_white = np.array([0, 0, 0])
+        high_white = np.array([180, 255, 30])
+        mask = cv2.inRange(hsv, low_white, high_white)
 
-        rho = 1  # distance resolution in pixels of the Hough grid
         theta = np.pi / 180  # angular resolution in radians of the Hough grid
-        threshold = 15  # minimum number of votes (intersections in Hough grid cell)
-        min_line_length = 50  # minimum number of pixels making up a line
-        max_line_gap = 20  # maximum gap in pixels between connectable line segments
+        threshold = 25  # minimum number of votes (intersections in Hough grid cell)
+        min_line_length = 100  # minimaal aantal pixels voor een lijn
+        max_line_gap = 50  # mmaximale gap in pixels tussen verbindbare lijn segmenten
 
-        # Run Hough on edge detected image
-        # Output "lines" is an array containing endpoints of detected line segments
-        lines = cv2.HoughLinesP(mask, rho, theta, threshold, np.array([]),
-                    min_line_length, max_line_gap)
+        lines = cv2.HoughLinesP(mask, 1, theta, threshold, np.array([]),
+                                min_line_length, max_line_gap)
 
         imgClone = img.copy()
         if lines is not None:
             for line in lines:
-                for x1,y1,x2,y2 in line:
-                    cv2.line(imgClone,(x1,y1),(x2,y2),(255,0,0),5)
+                for x1, y1, x2, y2 in line:
+                    cv2.line(imgClone, (x1, y1), (x2, y2), (0, 255, 255), 5)
 
-            lines_edges = cv2.addWeighted(img, 0.8, imgClone, 1, 0)
-        
         cv2.imshow('camservice-lijn', imgClone)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -55,4 +44,5 @@ def run():
     cv2.destroyAllWindows()
 
 
+print ("uncomment run before starting..")
 run()  # disabled for travis
