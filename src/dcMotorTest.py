1d81e3e97ca1a9bc7c83349d697c3beceff36c52
Jildert <jildert.venema@hotmail.com>
2018-05-15 13:47:16 +0200

    other pin

--- src/dcMotorTest.py
+++ src/dcMotorTest.py
@@ -4,15 +4,20 @@ import time
 from entities.movement.limb.dcmotor import DCMotor
 
 motor = DCMotor(18)
+othermotor = DCMotor(13)
 
 for cycle in range(0, 20):
     motor.forward(cycle, 0.5)
+    othermotor.forward(cycle, 0.5)
 
 for cycle in range(0, 20):
     motor.forward(20 - cycle, 0.5)
+    othermotor.forward(20 - cycle, 0.5)
 
 for cycle in range(0, 20):
     motor.backward(cycle, 0.5)
+    othermotor.forward(cycle, 0.5)
 
 for cycle in range(0, 20):
     motor.backward(20 - cycle, 0.5)
+    othermotor.forward(20 - cycle, 0.5)
