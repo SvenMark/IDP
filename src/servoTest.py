ecb8e3dd0e8e5fbf1d856b1f43c9ad12c32085fc
Jildert <jildert.venema@hotmail.com>
2018-05-15 11:55:59 +0200

    2 servos same time test

--- src/servoTest.py
+++ src/servoTest.py
@@ -4,8 +4,14 @@ from entities.movement.limb.servo import Servo
 import time
 
 servoprivod = Servo(31, 500)
+otherservo = Servo(13, 500)
 
+# should both move at the same time
 while True:
     servoprivod.move(500, 0)
+    otherservo.move(500, 0)
+
+    otherservo.move(300, 0)
     servoprivod.move(300, 0)
+
 
---
+++ src/temptest.py
@@ -0,0 +1,4 @@
+from libs.ax12 import Ax12
+
+ax12 = Ax12()
+print(ax12.ping(10))
