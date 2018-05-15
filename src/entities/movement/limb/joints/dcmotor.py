8abe108a167e2bb54faa90dadba74ff399cdc8a3
Rendall Schijven <rendall.schijven@hotmail.com>
2018-05-15 14:25:26 +0200

    Track class finished

--- src/entities/movement/limb/joints/dcmotor.py
+++ src/entities/movement/limb/joints/dcmotor.py
@@ -16,7 +16,7 @@ class DCMotor(object):
         self.pin_motor_forward = 10
         self.pin_motor_backward = 9
         self.pin_pwm = pin
-        self.frequency = 20
+        self.frequency = 60
         self.stop = 0
 
         GPIO.setup(self.pin_pwm, GPIO.OUT)
