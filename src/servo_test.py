91650a1d993e7d274194b62cca029dd443b5850f
Jildert <jildert.venema@hotmail.com>
2018-05-15 13:43:50 +0200

    other pin

--- src/servo_test.py
+++ src/servo_test.py
@@ -5,13 +5,15 @@ import time
 
 servoprivod = Servo(31, 500)
 otherservo = Servo(13, 500)
+anotherservo = Servo(63, 500)
 
 # should both move at the same time
 while True:
     servoprivod.move(500, 0)
     otherservo.move(500, 0)
+    anotherservo.move(500, 0)
 
     otherservo.move(300, 0)
     servoprivod.move(300, 0)
-
+    anotherservo.move(300, 0)
 
