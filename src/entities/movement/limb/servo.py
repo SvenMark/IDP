337d989e8bc73260de8ee92c3a31c7ad3b64a12c
Rendall Schijven <rendall.schijven@hotmail.com>
2018-05-15 10:12:17 +0200

    updated servo class

--- src/entities/movement/limb/servo.py
+++ src/entities/movement/limb/servo.py
@@ -14,14 +14,16 @@ class Servo(object):
         self.servo_id = servo_id
         self.last_position = initial_position
         self.ax12.move(servo_id, initial_position)
+        self.sensitivity = 4
 
     #Degrees are 0 - 998
     def move(self, degrees, delay):
-        while self.ax12.read_position(self.servo_id) != self.last_position:
-            time.sleep(0.2)
-
         if degrees < 0 or degrees > 998:
             print("In servo " + str(self.servo_id) + ", degrees: " + str(degrees) + ", must be between 0 and 998")
+
+        while self.ax12.read_position(self.servo_id) - self.last_position > self.sensitivity:
+            time.sleep(0.2)
+
         self.last_position = degrees
         self.ax12.move(self.servo_id, degrees)
         time.sleep(delay)
