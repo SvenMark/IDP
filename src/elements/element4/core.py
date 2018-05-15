b88ee7d20c8f97a93148de1f0feb3e0186c3f4b1
kevin <kevinsnijder9@hotmail.com>
2018-05-15 10:43:42 +0200

    Microphone Frequency Peak Visualisation

--- src/elements/element4/core.py
+++ src/elements/element4/core.py
@@ -5,21 +5,29 @@ import numpy as np
 def run():
     print("run element4")
 
-    CHUNK = 2 ** 11
-    RATE = 44100
-    RUNTIME = 30 #Time to run the script in seconds
-    SENSITIVITY = 10 #Sensitivity to sound, lower is more sensitive
+    np.set_printoptions(suppress=True)  # don't use scientific notation
 
-    p = pyaudio.PyAudio()
+    CHUNK = 2000  # speed of data point reading, smaller is less datapoints is faster updates
+    RATE = 44100  # time resolution of the recording device (Hz)
+    OUTPUTS = 12000  # number of outputs
+
+    p = pyaudio.PyAudio()  # start the PyAudio class
     stream = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True,
-                    frames_per_buffer=CHUNK)
+                    frames_per_buffer=CHUNK)  # uses default input device
 
-    for i in range(int(RUNTIME * 44100 / 1024)):  # go for a few seconds
+    for i in range(OUTPUTS):  # Number of outputs
         data = np.fromstring(stream.read(CHUNK), dtype=np.int16)
-        peak = np.average(np.abs(data)) * 2
-        bars = "#" * int(50 * peak / 2 ** SENSITIVITY)
-        print("%04d %05d %s" % (i, peak, bars))
+        data = data * np.hanning(len(data))  # smooth the FFT by windowing data
+        fft = abs(np.fft.fft(data).real)
+        fft = fft[:int(len(fft) / 2)]  # keep only first half
+        freq = np.fft.fftfreq(CHUNK, 1.0 / RATE)
+        freq = freq[:int(len(freq) / 2)]  # keep only first half
+        freqPeak = freq[np.where(fft == np.max(fft))[0][0]] + 1
+        #print("Peak frequency: %d Hz" % freqPeak)
+        bars = "#" * int(500 * freqPeak / 2 ** 16)
+        print("Peak: %05d hz %s" % (freqPeak, bars))
 
+    # close the stream
     stream.stop_stream()
     stream.close()
     p.terminate()
