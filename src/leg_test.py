2fbdb5b3b68070f3e541a71566c330b1e3676109
RendallSchijven <rendall.schijven@hotmail.com>
2018-05-16 13:20:17 +0000

    Updated leg tester

--- src/leg_test.py
+++ src/leg_test.py
@@ -3,7 +3,7 @@ from entities.movement.limb.leg import Leg
 leg = Leg()
 
 while True:
-    leg.move(998, 998, 998, 2)
-    leg.move(500, 500, 500, 2)
-    leg.move(0, 0, 0, 2)
-    leg.move(500, 500, 500, 2)
+    leg.move(400, 762, 20, 0)
+    leg.move(654, 123, 300, 0)
+    leg.move(11, 987, 666, 0)
+    leg.move(500, 500, 500, 0)
