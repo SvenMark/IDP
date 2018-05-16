afa6d3c1b63f64e6d4e2fb81e0b944c95c530af0
Rendall Schijven <rendall.schijven@hotmail.com>
2018-05-16 15:14:11 +0200

    updated values in leg test

--- src/leg_test.py
+++ src/leg_test.py
@@ -3,7 +3,7 @@ from entities.movement.limb.leg import Leg
 leg = Leg()
 
 while True:
-    leg.move(1000, 1000, 1000, 2)
+    leg.move(998, 998, 998, 2)
     leg.move(500, 500, 500, 2)
     leg.move(0, 0, 0, 2)
     leg.move(500, 500, 500, 2)
