818530be9cb39fe949f4c8de284879c509cec68c
Rendall Schijven <rendall.schijven@hotmail.com>
2018-05-16 09:27:52 +0200

    Updated tracks test and class

--- src/track_class_test.py
+++ src/track_class_test.py
@@ -1,3 +1,5 @@
+#!/bin/python
+
 from entities.movement.limb.tracks import Tracks
 
 tracks = Tracks()
@@ -6,10 +8,22 @@ for cycle in range(0, 20):
     tracks.forward(cycle, 0.5)
 
 for cycle in range(0, 20):
+    tracks.forward(20 - cycle, 0.5)
+
+for cycle in range(0, 20):
     tracks.backward(cycle, 0.5)
+
+for cycle in range(0, 20):
+    tracks.backward(20 - cycle, 0.5)
 
 for cycle in range(0, 20):
     tracks.turn_right(cycle, cycle, 0.5)
 
 for cycle in range(0, 20):
+    tracks.turn_right(20 - cycle, 20 - cycle, 0.5)
+
+for cycle in range(0, 20):
     tracks.turn_left(cycle, cycle, 0.5)
+
+for cycle in range(0, 20):
+    tracks.turn_left(20 - cycle, 20 - cycle, 0.5)
