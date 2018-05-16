85b22cd7c321e2e2a1e40cdb68a9034c5b1751dc
RendallSchijven <rendall.schijven@hotmail.com>
2018-05-16 11:04:44 +0000

    Updated track test to new track class

--- src/track_class_test.py
+++ src/track_class_test.py
@@ -5,6 +5,9 @@ from entities.movement.tracks import Tracks
 
 tracks = Tracks()
 
-tracks.forward(50, 0, 1)
-tracks.forward(20, 5, 1)
+tracks.forward(100, 2, 1)
+tracks.forward(20, 4, 4)
+tracks.forward(0, 1, 1)
+tracks.turn_right(70, 20, 8, 12)
+tracks.turn_right(10, 15, 8, 7)
 
