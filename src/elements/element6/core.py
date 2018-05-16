2d632b1343bd9e39ca74eb40feb6785544898557
AltijdMelvin <bos12bos@hotmail.com>
2018-05-16 15:12:12 +0200

    Average distance to edges

--- src/elements/element6/core.py
+++ src/elements/element6/core.py
@@ -1,6 +1,6 @@
-import random
-from collections import OrderedDict
 from imutils.video import WebcamVideoStream
+from helpers import Point
+from decimal import Decimal
 import time
 import numpy as np
 import cv2
@@ -14,6 +14,8 @@ def run():
     sample = cap.read()
     height = sample.shape[0]  # get height
     width = sample.shape[1]  # get width
+    print("w: " + str(width) + " " + "h: " + str(height))
+
     vertices = [
         (0, height),
         (width / 2, height / 2),
@@ -31,6 +33,7 @@ def run():
         high_black = np.array([180, 255, 30])
         mask = cv2.inRange(hsv, low_black, high_black)
 
+        # Get lines
         theta = np.pi / 180
         threshold = 150
         min_line_length = 40
@@ -39,12 +42,18 @@ def run():
         lines = cv2.HoughLinesP(mask, 6, theta, threshold, np.array([]),
                                 min_line_length, max_line_gap)
 
+        # Draw lines
+        linecolor = (255, 0, 0)
         imgClone = img.copy()
+
         if lines is not None:
             for line in lines:
                 for x1, y1, x2, y2 in line:
-                    cv2.line(imgClone, (x1, y1), (x2, y2), (255, 0, 0), 5)
-        
+                    p1 = Point(x1, y1)
+                    p2 = Point(x2, y2)
+                    cv2.line(imgClone, (p1.x, p1.y), (p2.x, p2.y), linecolor, 5)
+            averagedistance(lines, width)
+
         cv2.imshow('camservice-lijn', imgClone)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -68,6 +77,32 @@ def setregion(img, vertices):
     cv2.bitwise_not(bg, bg, mask=new_mask)
     
     return masked_img + bg
+
+
+def midpoint(p1, p2):
+    midp = Point(0, 0)
+    midp.x = (p1.x + p2.x) / 2
+    midp.y = (p1.y + p2.y) / 2
+    return midp
+
+
+def averagedistance(lines, width):
+    count = 0
+    totaldr = 0
+    totaldl = 0
+    for line in lines:
+        for x1, y1, x2, y2 in line:
+            p1 = Point(x1, y1)
+            p2 = Point(x2, y2)
+            midp = midpoint(p1, p2)
+            totaldr += (width - midp.x)      # Distance to right
+            totaldl += (0 + midp.x)  # Distance to right
+            count += 1
+    # Average to sides (x-as)
+    percentageleft = Decimal((Decimal(Decimal(totaldl) / Decimal(count)) / Decimal(width)) * Decimal(100))
+    percentageright = Decimal((Decimal(Decimal(totaldr) / Decimal(count)) / Decimal(width)) * Decimal(100))
+    print("Percentage left: " + str(round(percentageleft)) + "%")
+    print("Percentage right: " + str(round(percentageright)) + "%")
 
 
 print ("uncomment run before starting..")
