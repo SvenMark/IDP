aa4809a49c30ef34584d2d73a7d2d6e3a4521dfe
Rendall Schijven <rendall.schijven@hotmail.com>
2018-05-16 14:28:18 +0200

    Added constructor to leg class

--- src/entities/movement/limb/leg.py
+++ src/entities/movement/limb/leg.py
@@ -1,10 +1,21 @@
 from entities.movement.limb.limb import Limb
+from entities.movement.limb.joints.servo import Servo
 
 
 class Leg(Limb):
     def __init__(self):
-        super(Leg, self).__init__('leg')
-        self.type = 'leg'
+
+        # Define the id`s of the servo`s
+        servo_0_id = 13
+        servo_1_id = 31
+        servo_2_id = 63
+
+        # Create the servo instances with correct id and starting position
+        self.servo_0 = Servo(servo_0_id, 500)
+        self.servo_1 = Servo(servo_1_id, 500)
+        self.servo_2 = Servo(servo_2_id, 500)
+
+        print("Tracks setup")
 
     def forward(self):
         print('{} forward'.format(self.limb_type))
