31a36bebddcf96ad2f237a63e4a88ddf111ae840
Melvin <bos12bos@hotmail.com>
2018-05-16 08:55:19 +0000

    Added cropped triangle

--- src/elements/element6/core.py
+++ src/elements/element6/core.py
@@ -11,30 +11,40 @@ def run():
     cap = WebcamVideoStream(src=0).start()
     time.sleep(1)  # startup
 
+    sample = cap.read()
+    height = sample.shape[0]  # get height
+    width = sample.shape[1]  # get width
+    vertices = [
+        (0, height),
+        (width / 2, height / 2),
+        (width, height),
+    ]
+
     while True:
         img = cap.read()
+        imgCropped = setregion(img, np.array([vertices], np.int32))
         blur = cv2.GaussianBlur(img, (9, 9), 0)
         hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
 
         # Mask
-        low_white = np.array([0, 0, 0])
-        high_white = np.array([180, 255, 30])
-        mask = cv2.inRange(hsv, low_white, high_white)
+        low_black = np.array([0, 0, 0])
+        high_black = np.array([180, 255, 30])
+        mask = cv2.inRange(imgCropped, low_black, high_black)
 
-        theta = np.pi / 180  # angular resolution in radians of the Hough grid
-        threshold = 25  # minimum number of votes (intersections in Hough grid cell)
-        min_line_length = 100  # minimaal aantal pixels voor een lijn
-        max_line_gap = 50  # mmaximale gap in pixels tussen verbindbare lijn segmenten
-
-        lines = cv2.HoughLinesP(mask, 1, theta, threshold, np.array([]),
+        theta = np.pi / 60
+        threshold = 160
+        min_line_length = 40  
+        max_line_gap = 25
+        
+        lines = cv2.HoughLinesP(mask, 6, theta, threshold, np.array([]),
                                 min_line_length, max_line_gap)
 
         imgClone = img.copy()
         if lines is not None:
             for line in lines:
                 for x1, y1, x2, y2 in line:
-                    cv2.line(imgClone, (x1, y1), (x2, y2), (0, 255, 255), 5)
-
+                    cv2.line(imgClone, (x1, y1), (x2, y2), (255, 0, 0), 5)
+        
         cv2.imshow('camservice-lijn', imgClone)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -42,6 +52,30 @@ def run():
 
     cap.stop()
     cv2.destroyAllWindows()
+
+
+def setregion(img, vertices):
+    # Define a blank matrix that matches the image height/width.
+    mask = np.zeros_like(img)
+    
+    # Retrieve the number of color channels of the image.
+    channel_count = img.shape[2]
+    
+    # Create a match color with the same color channel counts.
+    match_mask_color = (255,) * channel_count
+      
+    # Fill inside the polygon
+    cv2.fillPoly(mask, vertices, match_mask_color)
+    
+    # Returning the image only where mask pixels match
+    masked_img = cv2.bitwise_and(img, mask)
+
+    new_mask = np.zeros(masked_img.shape[:2], np.uint8)
+
+    bg = np.ones_like(masked_img, np.uint8)*255
+    cv2.bitwise_not(bg, bg, mask=new_mask)
+    
+    return masked_img + bg
 
 
 print ("uncomment run before starting..")
