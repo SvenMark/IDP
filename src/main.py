e7bdfab6e4d87268e2c40fc269471dcb8cf0f763
Rendall Schijven <rendall.schijven@hotmail.com>
2018-05-17 14:27:54 +0200

    updated main

--- src/main.py
+++ src/main.py
@@ -9,28 +9,28 @@ from entities.robot.robot import Robot
 from entities.movement.limb.leg import Leg
 from entities.movement.limb.tire import Tire
 
-RESOURCES = os.path.dirname(os.path.abspath(__file__)) + '\\resources\\'
-
-FUNC_MAP = {
-    "1": element1.core,
-    "2": element2.core,
-    "3": element3.core,
-    "4": element4.core,
-    "5": element5.core,
-    "6": element6.core,
-    "7": element7.core,
-    "8": element8.core,
-    "9": element9.core,
-    "10": element10.core
-}
+# RESOURCES = os.path.dirname(os.path.abspath(__file__)) + '\\resources\\'
+#
+# FUNC_MAP = {
+#     "1": element1.core,
+#     "2": element2.core,
+#     "3": element3.core,
+#     "4": element4.core,
+#     "5": element5.core,
+#     "6": element6.core,
+#     "7": element7.core,
+#     "8": element8.core,
+#     "9": element9.core,
+#     "10": element10.core
+# }
 
 
 def main():
-    print(RESOURCES)
-    # print command line arguments
-    if len(sys.argv) < 2:
-        print("Please pass commandline args")
-        return sys.exit(2)
+    # print(RESOURCES)
+    # # print command line arguments
+    # if len(sys.argv) < 2:
+    #     print("Please pass commandline args")
+    #     return sys.exit(2)
 
     lights = []
 
