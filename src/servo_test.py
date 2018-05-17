302f1edd809676c545e35bea69cb489f14340107
Rendall Schijven <rendall.schijven@hotmail.com>
2018-05-17 11:13:17 +0200

    updated id`s

--- src/servo_test.py
+++ src/servo_test.py
@@ -4,14 +4,14 @@ import time
 
 from entities.movement.limb.joints.servo import Servo
 
-servo0 = Servo(13, 500)
-servo1 = Servo(31, 500)
-servo2 = Servo(63, 500)
-servo3 = Servo(14, 500)
-servo4 = Servo(18, 500)
-servo5 = Servo(61, 500)
-servo6 = Servo(53, 500)
-servo7 = Servo(1, 500)
+servo0 = Servo(1, 500)
+servo1 = Servo(13, 500)
+servo2 = Servo(14, 500)
+servo3 = Servo(21, 500)
+servo4 = Servo(31, 500)
+servo5 = Servo(53, 500)
+servo6 = Servo(61, 500)
+servo7 = Servo(63, 500)
 
 # should both move at the same time
 while True:
