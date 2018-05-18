e38346583d2f3ae0d1415db6cbcfdbf31260e9a6
Rendall Schijven <rendall.schijven@hotmail.com>
2018-05-18 08:52:34 +0200

    Updated DC motor frequency

--- src/entities/movement/limb/joints/dcmotor.py
+++ src/entities/movement/limb/joints/dcmotor.py
@@ -16,9 +16,9 @@ class DCMotor(object):
         self.pin_motor_forward = 10
         self.pin_motor_backward = 9
         self.pin_pwm = pin
-        self.frequency = 12000
+        self.frequency = 1000
         self.stop = 0
-        self.currentspeed = 0
+        self.current_speed = 0
 
         GPIO.setup(self.pin_pwm, GPIO.OUT)
         GPIO.setup(self.pin_motor_forward, GPIO.OUT)
@@ -32,7 +32,7 @@ class DCMotor(object):
     # Turn all motors off
     def stop_motor(self):
         self.pwm_motor.ChangeDutyCycle(self.stop)
-        self.currentspeed = 0
+        self.current_speed = 0
 
     # Turn both motors forwards
     def forward(self, duty_cycle, delay):
@@ -40,7 +40,7 @@ class DCMotor(object):
         GPIO.output(self.pin_motor_forward, GPIO.HIGH)
         GPIO.output(self.pin_motor_backward, GPIO.LOW)
         self.pwm_motor.ChangeDutyCycle(duty_cycle)
-        self.currentspeed = duty_cycle
+        self.current_speed = duty_cycle
         time.sleep(delay)
 
     def backward(self, duty_cycle, delay):
@@ -48,17 +48,17 @@ class DCMotor(object):
         GPIO.output(self.pin_motor_forward, GPIO.LOW)
         GPIO.output(self.pin_motor_backward, GPIO.HIGH)
         self.pwm_motor.ChangeDutyCycle(duty_cycle)
-        self.currentspeed = duty_cycle
+        self.current_speed = duty_cycle
         time.sleep(delay)
 
     def clean_up(self):
         self.stop_motor()
-        self.currentspeed = 0
+        self.current_speed = 0
         GPIO.cleanup()
 
 
 def main():
-    dvigatel = DCMotor()
+    dvigatel = DCMotor(16)
     dvigatel.clean_up()
 
 
