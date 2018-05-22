b17433f591ae75d755824e99257acef161847b45
Rendall Schijven <rendall.schijven@hotmail.com>
2018-05-22 09:15:13 +0200

    Updated legs for use of leg test

--- src/entities/movement/legs.py
+++ src/entities/movement/legs.py
@@ -13,10 +13,10 @@ class Legs(object):
         """
 
         # Initialise a leg for each corner of the robot
-        self.leg_front_left = Leg(leg_0_servos[0], leg_0_servos[1], leg_0_servos[2], 500, 500, 500)
-        self.leg_front_right = Leg(leg_1_servos[0], leg_1_servos[1], leg_1_servos[2], 500, 500, 500)
-        # self.leg_rear_left = Leg(leg_2_servos[0], leg_2_servos[1], leg_2_servos[2], 500, 500, 500)
-        # self.leg_rear_right = Leg(leg_3_servos[0], leg_3_servos[1], leg_3_servos[2], 500, 500, 500)
+        self.leg_front_left = Leg(leg_0_servos[0], leg_0_servos[1], leg_0_servos[2], 600, 450, 850)
+        # self.leg_front_right = Leg(leg_1_servos[0], leg_1_servos[1], leg_1_servos[2], 600, 450, 850)
+        # self.leg_rear_left = Leg(leg_2_servos[0], leg_2_servos[1], leg_2_servos[2], 600, 450, 850)
+        # self.leg_rear_right = Leg(leg_3_servos[0], leg_3_servos[1], leg_3_servos[2], 600, 450, 850)
 
         print("Legs setup")
 
@@ -31,7 +31,7 @@ class Legs(object):
         :return: None
         """
         self.leg_front_left.move(leg_0_moves[0], leg_0_moves[1], leg_0_moves[2], delay)
-        self.leg_front_right.move(leg_1_moves[0], leg_1_moves[1], leg_1_moves[2], delay)
+        # self.leg_front_right.move(leg_1_moves[0], leg_1_moves[1], leg_1_moves[2], delay)
         # self.leg_rear_left.move(leg_2_moves[0], leg_2_moves[1], leg_2_moves[2], delay)
         # self.leg_rear_right.move(leg_3_moves[0], leg_3_moves[1], leg_3_moves[2], delay)
 
@@ -48,7 +48,7 @@ class Legs(object):
         delay = 0.1
 
         self.leg_front_left.move(leg_0_deploy[0], leg_0_deploy[1], leg_0_deploy[2], delay)
-        self.leg_front_right.move(leg_1_deploy[0], leg_1_deploy[1], leg_1_deploy[2], delay)
+        # self.leg_front_right.move(leg_1_deploy[0], leg_1_deploy[1], leg_1_deploy[2], delay)
         # self.leg_rear_left.move(leg_2_deploy[0], leg_2_deploy[1], leg_2_deploy[2], delay)
         # self.leg_rear_right.move(leg_3_deploy[0], leg_3_deploy[1], leg_3_deploy[2], delay)
 
@@ -65,6 +65,6 @@ class Legs(object):
         delay = 0.1
         
         self.leg_front_left.move(leg_0_retract[0], leg_0_retract[1], leg_0_retract[2], delay)
-        self.leg_front_right.move(leg_1_retract[0], leg_1_retract[1], leg_1_retract[2], delay)
+        # self.leg_front_right.move(leg_1_retract[0], leg_1_retract[1], leg_1_retract[2], delay)
         # self.leg_rear_left.move(leg_2_retract[0], leg_2_retract[1], leg_2_retract[2], delay)
         # self.leg_rear_right.move(leg_3_retract[0], leg_3_retract[1], leg_3_retract[2], delay)
