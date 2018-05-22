2371a98d0b54d521cdaa236a9c3f5af9902e77ae
Rendall Schijven <rendall.schijven@hotmail.com>
2018-05-22 15:07:25 +0200

    Updated walking sequence

--- src/entities/movement/sequences/walking_sequences.py
+++ src/entities/movement/sequences/walking_sequences.py
@@ -2,14 +2,14 @@ from entities.movement.legs import Legs
 
 
 def walk_forward(legs, speed, repeat):
-    for i in xrange(repeat):
+    for i in range(repeat):
         legs.move(leg_0_moves=[430, 766, 850],
                   leg_1_moves=[650, 400, 400],
                   leg_2_moves=[400, 400, 400],
                   leg_3_moves=[600, 400, 400],
                   delay=0.1,
                   speed=speed)
-        legs.move(leg_0_moves=[430, 666, 850],
+        legs.move(leg_0_moves=[430, 766, 850],
                   leg_1_moves=[650, 400, 400],
                   leg_2_moves=[400, 400, 400],
                   leg_3_moves=[600, 400, 400],
@@ -21,7 +21,7 @@ def walk_forward(legs, speed, repeat):
                   leg_3_moves=[600, 400, 400],
                   delay=0.1,
                   speed=speed)
-        legs.move(leg_0_moves=[530, 666, 850],
+        legs.move(leg_0_moves=[530, 700, 850],
                   leg_1_moves=[650, 400, 400],
                   leg_2_moves=[400, 400, 400],
                   leg_3_moves=[600, 400, 400],
@@ -68,7 +68,7 @@ def enge_dab(legs, speed):
 
 
 def wave(legs, speed, repeat):
-    for i in xrange(repeat):
+    for i in range(repeat):
         legs.move(leg_0_moves=[527, 690, 970],
                   leg_1_moves=[650, 400, 400],
                   leg_2_moves=[400, 400, 400],
