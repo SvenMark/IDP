aeb3327e6173880e1433eec95e9bbb107a03ad93
Rendall Schijven <rendall.schijven@hotmail.com>
2018-05-22 14:19:59 +0200

    Added walking sequence and converted move to move_speed

--- src/entities/movement/limb/joints/servo.py
+++ src/entities/movement/limb/joints/servo.py
@@ -28,51 +28,6 @@ class Servo(object):
         self.sensitivity = 5
         time.sleep(0.1)
 
-    def is_ready(self):
-        """
-        Function that checks if a servo completed it`s last move
-        :return: Whether or not the servo has completed it`s last move
-        """
-        return abs(self.ax12.read_position(self.servo_id) - self.last_position) <= self.sensitivity
-
-    def move(self, degrees, delay):
-        """
-        Function that moves the servo using the ax12 library move function
-        :param degrees: Position to move to
-        :param delay: Time to wait after executing
-        :return: None
-        """
-
-        # If degrees are out of range print an error
-        if degrees < 0 or degrees > 998:
-            print("In servo " + str(self.servo_id) + ", degrees: " + str(degrees) + ", must be between 0 and 998")
-
-        # While the servo has not completed it last command wait a bit and check again.
-        while not self.is_ready():
-            time.sleep(0.1)
-
-        # Move the servo using the ax12 library with the servo id and degrees.
-        self.ax12.move(self.servo_id, degrees)
-
-        # Set the last position to the degrees.
-        self.last_position = degrees
-
-        time.sleep(delay)
-
-    def read_position(self):
-        """
-        Read the position of the servo
-        :return: Current position of this servo
-        """
-        return self.ax12.read_position(self.servo_id)
-
-    def read_speed(self):
-        """
-        Read the speed of the servo
-        :return: The servo speed
-        """
-        return self.ax12.read_speed(self.servo_id)
-
     def move_speed(self, degrees, delay, speed):
         """
         Function that moves the servo using the ax12 library move function
@@ -98,6 +53,26 @@ class Servo(object):
 
         time.sleep(delay)
 
+    def is_ready(self):
+        """
+        Function that checks if a servo completed it`s last move
+        :return: Whether or not the servo has completed it`s last move
+        """
+        return abs(self.ax12.read_position(self.servo_id) - self.last_position) <= self.sensitivity
+
+    def read_position(self):
+        """
+        Read the position of the servo
+        :return: Current position of this servo
+        """
+        return self.ax12.read_position(self.servo_id)
+
+    def read_speed(self):
+        """
+        Read the speed of the servo
+        :return: The servo speed
+        """
+        return self.ax12.read_speed(self.servo_id)
 
 def main():
     servo = Servo(13, 0)
---
+++ src/entities/movement/sequences/walking_sequences.py
@@ -0,0 +1,56 @@
+from entities.movement.legs import Legs
+
+def walk_forward(legs, speed):
+    while True:
+        legs.move(leg_0_moves=[430, 766, 850],
+                  leg_1_moves=[650, 400, 400],
+                  leg_2_moves=[400, 400, 400],
+                  leg_3_moves=[600, 400, 400],
+                  delay=0.1,
+                  speed=speed)
+        legs.move(leg_0_moves=[430, 666, 850],
+                  leg_1_moves=[650, 400, 400],
+                  leg_2_moves=[400, 400, 400],
+                  leg_3_moves=[600, 400, 400],
+                  delay=0.1,
+                  speed=speed)
+        legs.move(leg_0_moves=[530, 766, 850],
+                  leg_1_moves=[650, 400, 400],
+                  leg_2_moves=[400, 400, 400],
+                  leg_3_moves=[600, 400, 400],
+                  delay=0.1,
+                  speed=speed)
+        legs.move(leg_0_moves=[530, 666, 850],
+                  leg_1_moves=[650, 400, 400],
+                  leg_2_moves=[400, 400, 400],
+                  leg_3_moves=[600, 400, 400],
+                  delay=0.1,
+                  speed=speed)
+
+
+def walk_backward(legs, speed):
+    while True:
+        legs.move(leg_0_moves=[430, 766, 850],
+                  leg_1_moves=[650, 400, 400],
+                  leg_2_moves=[400, 400, 400],
+                  leg_3_moves=[600, 400, 400],
+                  delay=0.1,
+                  speed=speed)
+        legs.move(leg_0_moves=[430, 666, 850],
+                  leg_1_moves=[650, 400, 400],
+                  leg_2_moves=[400, 400, 400],
+                  leg_3_moves=[600, 400, 400],
+                  delay=0.1,
+                  speed=speed)
+        legs.move(leg_0_moves=[530, 766, 850],
+                  leg_1_moves=[650, 400, 400],
+                  leg_2_moves=[400, 400, 400],
+                  leg_3_moves=[600, 400, 400],
+                  delay=0.1,
+                  speed=speed)
+        legs.move(leg_0_moves=[530, 666, 850],
+                  leg_1_moves=[650, 400, 400],
+                  leg_2_moves=[400, 400, 400],
+                  leg_3_moves=[600, 400, 400],
+                  delay=0.1,
+                  speed=speed)
