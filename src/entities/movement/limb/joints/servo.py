bde7181af0d4ddc23210db3bc73050f50e3c085f
Rendall Schijven <rendall.schijven@hotmail.com>
2018-05-22 10:27:51 +0200

    Added a move speed function to servo

--- src/entities/movement/limb/joints/servo.py
+++ src/entities/movement/limb/joints/servo.py
@@ -20,11 +20,11 @@ class Servo(object):
         # Create an instance of the Ax12 servo class from the Ax12 library.
         self.ax12 = Ax12()
 
-        print("Setting servo " + servo_id + " to " + initial_position)
+        print("Setting servo " + str(servo_id) + " to " + str(initial_position))
         # Set the servo variables and move servo to initial position.
         self.servo_id = servo_id
         self.last_position = initial_position
-        # self.ax12.move(servo_id, initial_position)
+        self.ax12.move(servo_id, initial_position)
         self.sensitivity = 2
         time.sleep(0.1)
 
@@ -72,6 +72,30 @@ class Servo(object):
         :return: The servo speed
         """
         return self.ax12.read_speed(self.servo_id)
+
+    def move_speed(self, degrees, delay, speed):
+        """
+        Function that moves the servo using the ax12 library move function
+        :param degrees: Position to move to
+        :param delay: Time to wait after executing
+        :return: None
+        """
+
+        # If degrees are out of range print an error
+        if degrees < 0 or degrees > 998:
+            print("In servo " + str(self.servo_id) + ", degrees: " + str(degrees) + ", must be between 0 and 998")
+
+        # While the servo has not completed it last command wait a bit and check again.
+        while not self.is_ready():
+            time.sleep(0.1)
+
+        # Move the servo using the ax12 library with the servo id and degrees.
+        self.ax12.move_speed(self.servo_id, degrees, speed)
+
+        # Set the last position to the degrees.
+        self.last_position = degrees
+
+        time.sleep(delay)
 
 
 def main():
--- src/servo_test.py
+++ src/servo_test.py
@@ -5,4 +5,4 @@ servo0 = Servo(14, 600)
 servo1 = Servo(61, 450)
 servo2 = Servo(63, 850)
 
-servo0.read_speed()
+servo0.move_speed(550, 5, 2)
