43368e9bc6fc40e88f1c16de24bb049f37d4e2a7
Lars Schipper <lars-schipper@hotmail.com>
2018-05-24 12:09:52 +0200

    moved functions to helper

--- src/elements/element7/calibrate.py
+++ src/elements/element7/calibrate.py
@@ -2,7 +2,7 @@ from elements.element7.helpers import Color
 from elements.element7.helpers import Block
 from elements.element7.helpers import ColorRange
 from elements.element7.helpers import SavedBuildings as db
-from elements.element7.core import is_duplicate
+from elements.element7.core import calculate_mask
 
 import numpy as np
 import cv2
@@ -18,24 +18,22 @@ def run():
     print("run element7")
     cap = cv2.VideoCapture(0)
 
+    # initialize color ranges for detection
+    color_range = [Color("orange", [0, 100, 126], [10, 255, 204]),
+                   Color("yellow", [100, 100, 100], [30, 255, 255]),
+                   Color("red", [170, 100, 100], [190, 255, 255]),
+                   Color("green", [60, 100, 50], [90, 255, 255]),
+                   Color("blue", [33, 213, 42], [110, 255, 255])]
+
     while True:
         ret, img = cap.read()
         img = cv2.GaussianBlur(img, (9, 9), 0)
 
-        # initialize color ranges for detection
-        color_range = [Color("orange", [0, 100, 126], [10, 255, 204]),
-                       Color("yellow", [10, 100, 100], [30, 255, 255]),
-                       Color("red", [170, 100, 100], [190, 255, 255]),
-                       Color("green", [60, 100, 50], [90, 255, 255]),
-                       Color("blue", [33, 213, 42], [110, 255, 255])]
-
         if not CALIBRATED and len(POSITIONS) > 0:
-            print("calibrting")
             color_range[0] = calibrate(POSITIONS, 30)
 
         # calculate the masks
         mask = calculate_mask(img, color_range)
-        cv2.rectangle(mask, (230, 65), (400, 420), (255, 255, 255), 3)
 
         cv2.imshow('camservice', mask)
 
@@ -66,21 +64,6 @@ def calibrate(positions, sensitivity=50):
             return Color(color, lower, upper)
 
     return Color(Color, [50, 100, 126], [10, 255, 204])
-    # new_range = list()
-    # for i in range(len(color_range)):  # for each color range available
-    #     c = color_range[i]
-    #     c.lower[0] = 0
-    #     c.upper[0] = 20
-    #     while c.upper[0] < 255:  # while the color value is not higher than 255
-    #         if not calibrate_color(POSITIONS, sensitivity, c.color):
-    #             # try with other color range
-    #             c.lower[0] += 10
-    #             c.upper[0] += 10
-    #         else:  # calibrated color
-    #             new_range.append(Color(c.color, c.lower, c.upper))
-    #         print(c.color, (c.lower, c.upper))
-    #
-    # return new_range
 
 
 def calibrate_color(positions, sensitivity, color):
@@ -101,59 +84,5 @@ def calibrate_color(positions, sensitivity, color):
     return False
 
 
-def calculate_mask(img, color_range, conversion=cv2.COLOR_BGR2HSV):
-    hsv = cv2.cvtColor(img, conversion)
-
-    masks = list()
-    for i in range(len(color_range)):
-        c = color_range[i]
-        masks.append(ColorRange(c.color, cv2.inRange(hsv, c.lower, c.upper)))
-
-    img_mask = set_contours(masks[0].range, masks[0].color, img)
-    for i in range(1, len(masks)):
-        img_mask += set_contours(masks[i].range, masks[i].color, img)
-
-    return img_mask
-
-
-# sets contours for selected masks
-def set_contours(mask, color, img):
-    img_mask = cv2.bitwise_and(img, img, mask=mask)
-
-    ret, thresh = cv2.threshold(mask, 127, 255, 0)
-    im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    # draw all correct contours
-    for i in range(len(contours)):
-        c = cv2.convexHull(contours[i])
-        peri = cv2.arcLength(c, True)
-        approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-        area = cv2.contourArea(c)
-
-        if len(approx) == 4 and area > 4000:
-            moment = cv2.moments(c)
-            cx = int(moment['m10'] / moment['m00'])
-            cy = int(moment['m01'] / moment['m00'])
-            cv2.drawContours(img_mask, [c], 0, (255, 255, 255), 3)
-            text = "{} {}".format("Color:", color)
-            cv2.putText(img_mask, text, (cx - 25, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (255, 255, 255), 1)
-            cv2.circle(img_mask, (cx, cy), 2, (255, 255, 255), 5)
-
-            if not STOP_POSITIONS:
-                if len(POSITIONS) > 10:
-                    print("Cleared POSITIONS of length ", len(POSITIONS))
-                    del POSITIONS[:]
-                if len(POSITIONS) == 0:
-                    POSITIONS.append(Block(color, (cx, cy)))
-                    print("Block(\"{}\", {}),".format(color, (cx, cy)))
-                else:
-                    if len(POSITIONS) > 0:
-                        for j in range(len(POSITIONS)):
-                            if not is_duplicate((cx, cy), POSITIONS, 5):
-                                POSITIONS.append(Block(color, (cx, cy)))
-                                print("Block(\"{}\", {}),".format(color, (cx, cy)))
-
-    return img_mask
-
-
-run()  # disabled for travis
+if __name__ == '__main__':
+    run()  # disabled for travis
--- src/elements/element7/core.py
+++ src/elements/element7/core.py
@@ -1,9 +1,11 @@
 from elements.element7.helpers import Color
 from elements.element7.helpers import Block
 from elements.element7.helpers import SavedBuildings as db
+from elements.element7.helpers import crop_to_contours, \
+                                      check_valid_convex, \
+                                      is_duplicate
 from entities.audio.speak import Speak
 
-import numpy as np
 import cv2
 
 POSITIONS = []
@@ -72,123 +74,6 @@ def calculate_mask(img, color_range, conversion=cv2.COLOR_BGR2HSV, set_contour=F
 
     # Return the new mask
     return img_mask
-
-
-def crop_to_contours(mask, img):
-    """
-    Crops to the contours of the given mask
-    :param mask: The mask to crop to
-    :param img: The image to crop
-    :return: The cropped image
-    """
-
-    # Create the threshold for the mask
-    ret, thresh = cv2.threshold(mask, 127, 255, 0)
-
-    # Find the contours for the threshold
-    im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    # Define the extreme points
-    extremes = [999999, -99999, 99999, -9999]  # min x, max x, min y, max y
-
-    for contour in range(len(contours)):
-        cnt = contours[contour]
-
-        # Get convexhull of the contour
-        c = cv2.convexHull(cnt)
-
-        # Check if the convex is a valid block
-        if check_valid_convex(c):
-            # Calculate extremes of the hull
-            min_x = tuple(cnt[cnt[:, :, 0].argmin()][0])
-            max_x = tuple(cnt[cnt[:, :, 0].argmax()][0])
-            min_y = tuple(cnt[cnt[:, :, 1].argmin()][0])
-            max_y = tuple(cnt[cnt[:, :, 1].argmax()][0])
-
-            # Compare with last extremes
-            if min_x[0] < extremes[0]:
-                extremes[0] = min_x[0]
-            if max_x[0] > extremes[1]:
-                extremes[1] = max_x[0]
-            if min_y[1] < extremes[2]:
-                extremes[2] = min_y[1]
-            if max_y[1] > extremes[3]:
-                extremes[3] = max_y[1]
-
-    # Calculate width and height
-    y = extremes[2]
-    h = extremes[3] - y
-    x = extremes[0]
-    w = extremes[1] - x
-
-    # Set new size
-    if y + h > 0 and x + w > 0:
-        img = img[y:y + h, x:x + w]
-
-    # Resize to new size
-    img = image_resize(img, height=400)
-
-    # Return the resized image
-    return img
-
-
-def check_valid_convex(c):
-    """
-    Checks if a convex is a valid block
-    :return: True if the contour is a block
-    """
-
-    # Calculate the perimeter
-    peri = cv2.arcLength(c, True)
-
-    # Calculate the sides of the convexhull, 4 is a square/rectangle, 3 is a triangle etc.
-    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-
-    # Calculate the area of the convexhull
-    area = cv2.contourArea(c)
-
-    # If the convexhull counts 4 sides and an area bigger than 4000
-    return len(approx) == 4 and area > 4000
-
-
-def image_resize(img, width=None, height=None, inter=cv2.INTER_AREA):
-    """
-    Resize the image to given size
-    :param img: Current image
-    :param width: The width to resize to
-    :param height: The height to resize to
-    :param inter: Resampling method using pixel area relation
-    :return: The resized image
-    """
-    # Initialize the dimensions of the image to be resized and
-    # grab the image size
-    dim = None
-    (h, w) = img.shape[:2]
-
-    # If both the width and height are None, then return the
-    # original image
-    if width is None and height is None:
-        return img
-
-    # Check to see if the width is None
-    if width is None:
-        # Calculate the ratio of the height and construct the
-        # dimensions
-        r = height / float(h)
-        dim = (int(w * r), height)
-
-    # Otherwise, the height is None
-    else:
-        # Calculate the ratio of the width and construct the
-        # dimensions
-        r = width / float(w)
-        dim = (width, int(h * r))
-
-    # Resize the image
-    resized = cv2.resize(img, dim, interpolation=inter)
-
-    # Return the resized image
-    return resized
 
 
 def set_contours(mask, color, img):
@@ -325,30 +210,6 @@ def recognize_building(positions):
 
     # Return whether a building has been found
     return found
-
-
-def is_duplicate(centre, positions, sensitivity=10, color=None):
-    """
-    Compares a centre points to an array with currently visible center points
-    :param centre: Centre point to check
-    :param positions: Array with blocks
-    :param sensitivity: Sensitivity to check with, higher sensitivity allows more distance
-    :param color: The color to compare, leave empty for no color
-    :return: True if array 'y' contains the centre point
-    """
-
-    for block in range(len(positions)):
-        # Calculate distance between the centre points
-        distance = np.linalg.norm(centre - positions[block].centre)  # x[i] = [520,137] y[t] = [430,180]
-
-        # If there is a given color, check those and
-        # the distance between the blocks
-        if color and color == positions[block].color and distance <= sensitivity:
-            return True
-        elif not color and distance <= sensitivity:
-            return True
-
-    return False
 
 
 if __name__ == '__main__':
