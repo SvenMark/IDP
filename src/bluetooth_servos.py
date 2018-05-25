8a479faca8aaab6efdcbe7e7d3f067398a3a1d64
Rendall Schijven <rendall.schijven@hotmail.com>
2018-05-25 11:56:07 +0200

    Updated naming

--- src/bluetooth_servos.py
+++ src/bluetooth_servos.py
@@ -13,22 +13,7 @@ legs = Legs(leg_0_servos=[
     14,
     61,
     63
-],
-    leg_1_servos=[
-        21,
-        31,
-        53
-    ],
-    leg_2_servos=[
-        61,
-        63,
-        111
-    ],
-    leg_3_servos=[
-        111,
-        111,
-        111
-    ])
+])
 tracks = Tracks(track_0_pin=18, track_1_pin=13)
 
 
