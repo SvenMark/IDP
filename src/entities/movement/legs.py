8793dc3ea6575078f4d8673b05942056d146c830
Rendall Schijven <rendall.schijven@hotmail.com>
2018-05-25 14:07:37 +0200

    Updated speeds

--- src/entities/movement/legs.py
+++ src/entities/movement/legs.py
@@ -95,12 +95,12 @@ class Legs(object):
                           leg_3_moves=[600, 400, 400],
                           delay=0,
                           speeds=[300, 300, 300])
-            y_axis = y_axis * 0.7
             if y_axis > 530:
-                walk_forward(self, [y_axis - 512, y_axis - 512, y_axis - 512])
+                walk_forward(self, [(y_axis - 512) * 0.7, (y_axis - 512) * 0.7, (y_axis - 512) * 0.7])
             if y_axis < 500:
-                walk_backward(self, [512 - y_axis, 512 - y_axis, 512 - y_axis])
+                walk_backward(self, [(512 - y_axis) * 0.7, (512 - y_axis) * 0.7, (512 - y_axis) * 0.7])
 
+            # Move according to joystick direction
             # self.move([530 + round(x_axis / 10), 680, 760 + round(y_axis / 10)],
             #           [650, 400, 400],
             #           [400, 400, 400],
