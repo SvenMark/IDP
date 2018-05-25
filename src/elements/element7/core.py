7ac68d26ae8c727d888964f4eccce8ad1d2f65d0
Lars Schipper <lars-schipper@hotmail.com>
2018-05-25 14:45:23 +0200

    calibrating before recognizing

--- src/elements/element7/core.py
+++ src/elements/element7/core.py
@@ -1,4 +1,5 @@
 from entities.vision.camera import Camera
+from entities.vision.calibrate import Calibrate
 from entities.vision.helpers import Color
 
 # Initialize color ranges for detection
@@ -8,5 +9,8 @@ color_range = [Color("orange", [0, 100, 100], [12, 255, 255]),
                Color("green", [71, 89, 11], [83, 202, 120]),
                Color("blue", [99, 152, 128], [119, 228, 174])]
 
+
+cali = Calibrate(color_range)
+color_range = cali.run()
 cam = Camera(color_range)
 cam.run()
