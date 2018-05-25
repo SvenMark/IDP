225e226abd34a873d34f4663f5e10c66e429309d
Rendall Schijven <rendall.schijven@hotmail.com>
2018-05-25 11:50:12 +0200

    Commented unused elements

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import os
 import sys
 
-from elements import element1, element2, element3, element4, element5, element6, element7, element8, element9, \
-    element10
+# from elements import element1, element2, element3, element4, element5, element6, element7, element8, element9, \
+#    element10
 from entities.movement.legs import Legs
 from entities.movement.tracks import Tracks
 from entities.robot.robot import Robot
@@ -11,26 +11,26 @@ from entities.movement.sequences.walking_sequences import *
 
 RESOURCES = os.path.dirname(os.path.abspath(__file__)) + '\\resources\\'
 
-FUNC_MAP = {
-    "1": element1.core,
-    "2": element2.core,
-    "3": element3.core,
-    "4": element4.core,
-    "5": element5.core,
-    "6": element6.core,
-    "7": element7.core,
-    "8": element8.core,
-    "9": element9.core,
-    "10": element10.core
-}
+#FUNC_MAP = {
+#    "1": element1.core,
+#    "2": element2.core,
+#    "3": element3.core,
+#    "4": element4.core,
+#    "5": element5.core,
+#    "6": element6.core,
+#    "7": element7.core,
+#    "8": element8.core,
+#    "9": element9.core,
+#    "10": element10.core
+#}
 
 
 def main():
-    print(RESOURCES)
+    #print(RESOURCES)
     # print command line arguments
-    if len(sys.argv) < 2:
-        print("Please pass commandline args")
-        return sys.exit(2)
+    #if len(sys.argv) < 2:
+        #print("Please pass commandline args")
+        #return sys.exit(2)
 
     limbs = [
             Legs(leg_0_servos=[
