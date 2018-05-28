69e6fec38b42543d5087f5e7272360b77b922734
Jildert <jildert.venema@hotmail.com>
2018-05-28 10:50:39 +0200

    ready fix

--- src/entities/movement/legs.py
+++ src/entities/movement/legs.py
@@ -35,7 +35,7 @@ class Legs(object):
         
     def move(self, leg_0_moves, leg_1_moves, leg_2_moves, leg_3_moves, delay, speeds):
         """
-        Function to move the legs together
+        Function to move the legs_not_ready together
         :param leg_0_moves: Array of positions for leg 0
         :param leg_1_moves: Array of positions for leg 1
         :param leg_2_moves: Array of positions for leg 2
@@ -52,15 +52,15 @@ class Legs(object):
         # self.leg_rear_left.move(leg_2_moves[0], leg_2_moves[1], leg_2_moves[2], delay)
         # self.leg_rear_right.move(leg_3_moves[0], leg_3_moves[1], leg_3_moves[2], delay)
 
-        # while legs are not ready, update
-        legs = [elem for elem in self.legs if elem.ready()]
-        while len(legs) != 3:
-            for i in range(len(legs)):
+        # while legs_not_ready are not ready, update
+        legs_not_ready = [elem for elem in self.legs if not elem.ready()]
+        while len(legs_not_ready) != 0:
+            for i in range(len(legs_not_ready)):
                 next_time = datetime.datetime.now()
                 elapsed_time = next_time - previous
                 previous = next_time
-                legs[i].update(elapsed_time.total_seconds())
-            legs = [elem for elem in self.legs if elem.ready()]
+                legs_not_ready[i].update(elapsed_time.total_seconds())
+            legs_not_ready = [elem for elem in self.legs if not elem.ready()]
 
     def deploy(self, speed):
         """
