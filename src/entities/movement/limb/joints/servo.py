4fd3baa784f49b77c6cfe72243cf286f576ad234
Jildert <jildert.venema@hotmail.com>
2018-05-28 10:21:34 +0200

    speed fix

--- src/entities/movement/limb/joints/servo.py
+++ src/entities/movement/limb/joints/servo.py
@@ -30,6 +30,8 @@ class Servo(object):
         self.goal = initial_position
         self.current_speed = 200
 
+        self.start_position = self.last_position
+
         self.ax12.move_speed(servo_id, initial_position, 300)
 
         self.sensitivity = 5
@@ -39,12 +41,13 @@ class Servo(object):
 
     def update(self, delta):
         # move towards new position
-        self.last_position = self.last_position + (self.goal - self.last_position) * delta * self.current_speed * 0.01
-        self.ax12.move(self.servo_id, self.last_position)
+        self.last_position = self.last_position + (self.goal - self.start_position) * delta * self.current_speed * 0.01
+        self.ax12.move(self.servo_id, round(self.last_position))
         print(str(self.last_position))
 
     def move(self, degrees, delay, speed):
         self.last_position = self.ax12.read_position(self.servo_id)
+        self.start_position = self.last_position
         self.goal = degrees
         self.current_speed = speed
 
