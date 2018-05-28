76ef71b2ca0c27314cbc022a6c03eb411c4d5847
Jildert <jildert.venema@hotmail.com>
2018-05-28 10:33:49 +0200

    faster

--- src/entities/movement/limb/joints/servo.py
+++ src/entities/movement/limb/joints/servo.py
@@ -41,7 +41,7 @@ class Servo(object):
 
     def update(self, delta):
         # move towards new position
-        self.last_position = self.last_position + (self.goal - self.start_position) * delta * self.current_speed * 0.0001
+        self.last_position = self.last_position + (self.goal - self.start_position) * delta * self.current_speed * 0.1
         self.ax12.move(self.servo_id, round(self.last_position))
 
         print("Moving " + str(self.last_position) + ", to " + str(self.goal))
