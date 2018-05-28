7ba03303db9e3e79ae6ba26e745a440312e812dd
Jildert <jildert.venema@hotmail.com>
2018-05-28 11:10:23 +0200

    repeat

--- src/functie_demo.py
+++ src/functie_demo.py
@@ -28,7 +28,7 @@ legs = Legs(leg_0_servos=[
             )
 
 # legs.deploy(150)
-walk_forward(legs, [200,200,200])
+walk_forward_repeat(legs, [200,200,200], 10)
 wave(legs, [250, 250, 250], 10)
 enge_dab(legs, [140, 140, 140])
 time.sleep(1)
