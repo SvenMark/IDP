89ec08adca1ebbf2b67aea02ec0f97ed2d96db81
Rendall Schijven <rendall.schijven@hotmail.com>
2018-05-29 14:11:01 +0200

    added high

--- src/entities/movement/sequences/walking_sequences.py
+++ src/entities/movement/sequences/walking_sequences.py
@@ -123,3 +123,9 @@ def pull(legs, speeds, ):
               leg_3_moves=[600, 400, 400],
               delay=0,
               speeds=speeds)
+    legs.move(leg_0_moves=[530, 650, 970],
+              leg_1_moves=[650, 400, 400],
+              leg_2_moves=[400, 400, 400],
+              leg_3_moves=[600, 400, 400],
+              delay=0,
+              speeds=speeds)
