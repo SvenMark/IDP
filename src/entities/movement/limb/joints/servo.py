14010ecc0382f56497adbd2be8e7ede68f091b3f
Jildert <jildert.venema@hotmail.com>
2018-05-29 10:42:19 +0200

    haakjes

--- src/entities/movement/limb/joints/servo.py
+++ src/entities/movement/limb/joints/servo.py
@@ -43,7 +43,7 @@ class Servo(object):
     def update(self, delta):
         # move towards new position
         step = (self.goal - self.start_position) * delta * self.current_speed
-        self.last_position = self.last_position + math.sin(step / self.goal * math.pi) * step
+        self.last_position = self.last_position + (math.sin(step / self.goal * math.pi) * step)
         self.ax12.move(self.servo_id, round(self.last_position))
 
     def move(self, degrees, delay, speed):
