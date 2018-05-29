ef3b3b7862b5e1f98f9b205367a9af7284302037
Rendall Schijven <rendall.schijven@hotmail.com>
2018-05-29 15:07:44 +0200

    Reverted servo and updated retract

--- src/entities/movement/limb/joints/servo.py
+++ src/entities/movement/limb/joints/servo.py
@@ -28,7 +28,7 @@ class Servo(object):
         self.servo_id = servo_id
         self.last_position = self.ax12.read_position(self.servo_id)
         self.goal = initial_position
-        self.current_speed = 200.0
+        self.current_speed = 200
         self.current_speed_multiplier = 0.01
 
         self.start_position = self.last_position
@@ -50,7 +50,7 @@ class Servo(object):
         self.last_position = self.ax12.read_position(self.servo_id)
         self.start_position = self.last_position
         self.goal = degrees
-        self.current_speed = float(speed) * float(self.current_speed_multiplier)
+        self.current_speed = speed * self.current_speed_multiplier
         print("servo " + str(self.servo_id) + ", start: " + str(self.last_position) + ", goal: " + str(self.goal))
 
     def move_speed(self, degrees, delay, max_speed):
