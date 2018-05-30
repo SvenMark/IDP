b0761cf1d9f7bbb1049a23ea94d5c44ccad9c6c0
Jildert <jildert.venema@hotmail.com>
2018-05-30 14:12:58 +0200

    lib test

--- src/libs/inverse_kinematics/test_kinematics.py
+++
@@ -1,8 +0,0 @@
-from actuator import Actuator
-from point import Point3D
-
-actuator = Actuator()
-
-rotations = actuator.inverse_kinematics(point=Point3D(90, 0, 0))
-
-print(str(rotations))
---
+++ src/libs/robot/test.py
@@ -0,0 +1,5 @@
+import kinematics
+import Robot
+import Link
+
+robot = Robot(Link(10, 1, 10), Link(20, 1, 10))
