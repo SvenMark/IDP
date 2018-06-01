3c58878f19d0650c55d70e5560c5d3bae916c7f9
Rendall Schijven <rendall.schijven@hotmail.com>
2018-06-01 11:09:21 +0200

    test decoding

--- src/entities/connection/bluetooth_controller.py
+++ src/entities/connection/bluetooth_controller.py
@@ -13,7 +13,7 @@ class BluetoothController(object):
         :param limbs: Array of robot limbs
         """
         self.bluetooth_address = bluetooth_address
-        # self.legs = limbs[0]
+        self.legs = limbs[0]
         self.tracks = limbs[1]
 
     def receive_data(self):
