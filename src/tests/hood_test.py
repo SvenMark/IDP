120beb66aff1acc14c62cfca6e1dda166c54376a
Rendall Schijven <rendall.schijven@hotmail.com>
2018-06-04 10:36:17 +0200

    Added hood handshake

---
+++ src/tests/hood_test.py
@@ -0,0 +1,33 @@
+import time
+import sys
+
+sys.path.insert(0, '../../src')
+
+from entities.movement.legs import Legs
+from entities.movement.sequences.sequences import *
+
+legs = Legs(leg_0_servos=[
+                14,
+                61,
+                63
+            ]
+#            leg_1_servos=[
+#                21,
+#                31,
+#                53
+#            ],
+#            leg_2_servos=[
+#                61,
+#                63,
+#                111
+#            ],
+#            leg_3_servos=[
+#                111,
+#                111,
+#                111
+#            ]
+            )
+
+
+legs.run_sequence([150, 150, 150], self_update=True, sequences=None, sequence=hood_handshake)
+
