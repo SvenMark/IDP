bc15292cca826670c54c0de246e2660af20c5ddb
Lars Schipper <lars-schipper@hotmail.com>
2018-06-06 09:56:54 +0200

    use convexhull instead of contour

--- src/elements/element7/core.py
+++ src/elements/element7/core.py
@@ -6,7 +6,7 @@ import time
 from entities.vision.vision import Vision
 from entities.vision.helpers.vision_helper import Color
 from entities.vision.recognize_settings import Recognize_settings
-from entities.movement.tracks import Tracks
+# from entities.movement.tracks import Tracks
 
 # Initialize color ranges for detection
 color_range = [Color("orange", [0, 69, 124], [13, 255, 255]),
@@ -39,38 +39,42 @@ settings = Recognize_settings()
 vision = Vision(color_range=color_range_test_room,
                 saved_buildings=saved_buildings,
                 settings=settings)
-if sys.argv[1] == "hsv_picker":
-    threading.Thread(target=vision.helpers.hsv_picker.run).start()
-    print("Starting: ", sys.argv[1])
-elif sys.argv[1] == "save":
-    threading.Thread(target=vision.saving.run).start()
-    print("Starting: ", sys.argv[1])
+if len(sys.argv) > 1:
+    if sys.argv[1] == "hsv_picker":
+        threading.Thread(target=vision.helpers.hsv_picker.run).start()
+        print("Starting: ", sys.argv[1])
+    elif sys.argv[1] == "save":
+        threading.Thread(target=vision.saving.run).start()
+        print("Starting: ", sys.argv[1])
+    else:
+        threading.Thread(target=vision.helpers.hsv_picker.run).start()
+        print("Starting: recognize")
 else:
     threading.Thread(target=vision.helpers.hsv_picker.run).start()
     print("Starting: recognize")
 
 rotate_speed = 50
 
-
-# TESTING
-tracks = Tracks(track_0_pin=18,
-                track_1_pin=13,
-                track_0_forward=22,
-                track_0_backward=27,
-                track_1_forward=10,
-                track_1_backward=9)
-
-while True:
-    if settings.update:
-        settings.update = False
-        if settings.new:
-            tracks.stop()
-            print("Moving to building " + str(settings.current_building)
-                  + ", position: " + str(settings.current_position))
-
-            settings.new = False
-        else:
-            print("Rotating")
-            # acceleration 0.5 seconds for 0.5 seconds, then wait again
-            tracks.turn_left(rotate_speed, rotate_speed, 0.5, 0.5)
-            tracks.stop()
+#
+# # TESTING
+# tracks = Tracks(track_0_pin=18,
+#                 track_1_pin=13,
+#                 track_0_forward=22,
+#                 track_0_backward=27,
+#                 track_1_forward=10,
+#                 track_1_backward=9)
+#
+# while True:
+#     if settings.update:
+#         settings.update = False
+#         if settings.new:
+#             tracks.stop()
+#             print("Moving to building " + str(settings.current_building)
+#                   + ", position: " + str(settings.current_position))
+#
+#             settings.new = False
+#         else:
+#             print("Rotating")
+#             # acceleration 0.5 seconds for 0.5 seconds, then wait again
+#             tracks.turn_left(rotate_speed, rotate_speed, 0.5, 0.5)
+#             tracks.stop()
