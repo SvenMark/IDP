7f643a3d33236af6f2462b40d2623aae14c45a75
AltijdMelvin <bos12bos@hotmail.com>
2018-06-07 15:19:02 +0200

    Added better cup detection and distance

--- src/elements/element5/core.py
+++ src/elements/element5/core.py
@@ -1,4 +1,5 @@
 import sys
+
 sys.path.insert(0, '../../../src')
 
 from entities.vision.recognize import Recognize, Block
@@ -6,28 +7,27 @@ from entities.vision.helpers.helpers import Color, Building
 
 # Required to call the Camera function
 saved_buildings = saved_buildings = [
-        Building(front=[Block("orange", (41, 324)),
-                        Block("yellow", (33, 97)),
-                        Block("red", (148, 92)),
-                        Block("green", (153, 318)),
-                        Block("blue", (92, 218))],
-                 back=[Block("blue", (31, 316)),
-                       Block("green", (86, 209)),
-                       Block("orange", (30, 91)),
-                       Block("yellow", (144, 317))],
-                 left=[Block("red", (112, 175)),
-                       Block("blue", (44, 304)),
-                       Block("green", (36, 68)),
-                       Block("orange", (184, 70)),
-                       Block("yellow", (180, 307))],
-                 right=[Block("red", (112, 175)),
-                        Block("blue", (44, 304)),
-                        Block("green", (36, 68)),
-                        Block("orange", (184, 70)),
-                        Block("yellow", (180, 307))]
-                 )
-    ]
-
+    Building(front=[Block("orange", (41, 324)),
+                    Block("yellow", (33, 97)),
+                    Block("red", (148, 92)),
+                    Block("green", (153, 318)),
+                    Block("blue", (92, 218))],
+             back=[Block("blue", (31, 316)),
+                   Block("green", (86, 209)),
+                   Block("orange", (30, 91)),
+                   Block("yellow", (144, 317))],
+             left=[Block("red", (112, 175)),
+                   Block("blue", (44, 304)),
+                   Block("green", (36, 68)),
+                   Block("orange", (184, 70)),
+                   Block("yellow", (180, 307))],
+             right=[Block("red", (112, 175)),
+                    Block("blue", (44, 304)),
+                    Block("green", (36, 68)),
+                    Block("orange", (184, 70)),
+                    Block("yellow", (180, 307))]
+             )
+]
 
 
 def run():
@@ -36,16 +36,64 @@ def run():
 
 
 def detect_cup():
+    import cv2
+    import numpy as np
 
-    # Initialize color ranges for detection
-    color_range = [Color("beker", [23, 48, 24], [44, 255, 255])]
+    MIN_MATCH_COUNT = 20
 
-    cam = Recognize(color_range, saved_buildings=None)
-    cam.run()
+    detector = cv2.xfeatures2d.SIFT_create()
+
+    FLANN_INDEX_KDITREE = 0
+    flannParam = dict(algorithm=FLANN_INDEX_KDITREE, tree=5)
+    flann = cv2.FlannBasedMatcher(flannParam, {})
+
+    trainImg = cv2.imread("ding.jpg", 0)
+    trainKP, trainDesc = detector.detectAndCompute(trainImg, None)
+
+    cam = cv2.VideoCapture(0)
+    while True:
+        ret, QueryImgBGR = cam.read()
+        QueryImg = cv2.cvtColor(QueryImgBGR, cv2.COLOR_BGR2GRAY)
+        queryKP, queryDesc = detector.detectAndCompute(QueryImg, None)
+        matches = flann.knnMatch(queryDesc, trainDesc, k=2)
+
+        goodMatch = []
+        for m, n in matches:
+            if m.distance < 0.75 * n.distance:
+                goodMatch.append(m)
+        if len(goodMatch) > MIN_MATCH_COUNT:
+            tp = []
+            qp = []
+            for m in goodMatch:
+                tp.append(trainKP[m.trainIdx].pt)
+                qp.append(queryKP[m.queryIdx].pt)
+            tp, qp = np.float32((tp, qp))
+            H, status = cv2.findHomography(tp, qp, cv2.RANSAC, 3.0)
+            h, w = trainImg.shape
+            trainBorder = np.float32([[[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]])
+            queryBorder = cv2.perspectiveTransform(trainBorder, H)
+
+            moment = cv2.moments(queryBorder)
+
+            x, y, w, h = cv2.boundingRect(queryBorder)
+            distance = 0.00008650519031141868 * h**2 - 0.10294117647058823 * h + 35
+            # Calculate the centre of mass
+            cx = int(moment['m10'] / moment['m00'])
+            cy = int(moment['m01'] / moment['m00'])
+
+            cv2.putText(QueryImgBGR, "Cup", (cx - 30, cy), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
+            cv2.polylines(QueryImgBGR, [np.int32(queryBorder)], True, (255, 255, 255), 4)
+            print("Cup detected at distance: " + str(distance) + "cm")
+        else:
+            print("Not Enough match found- %d/%d" % (len(goodMatch), MIN_MATCH_COUNT))
+        cv2.imshow('result', QueryImgBGR)
+        if cv2.waitKey(10) == ord('q'):
+            break
+    cam.release()
+    cv2.destroyAllWindows()
 
 
 def detect_bridge():
-
     # Initialize color ranges for detection
     color_range = [Color("Brug", [0, 0, 0], [0, 255, 107]),
                    Color("Gat", [0, 0, 0], [0, 0, 255]),
