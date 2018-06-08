c4435cf76fa796dd3723ad571f39b063f54964ad
Rendall Schijven <rendall.schijven@hotmail.com>
2018-06-08 15:15:59 +0200

    Print forward

--- src/entities/movement/tracks.py
+++ src/entities/movement/tracks.py
@@ -188,6 +188,7 @@ class Tracks(object):
 
             # Move forward
             if vertical_speed > dead_zone:
+                print("Forward")
                 if -dead_zone < horizontal_speed < dead_zone:
                     self.forward(duty_cycle_track_left=vertical_speed,
                                  duty_cycle_track_right=vertical_speed,
