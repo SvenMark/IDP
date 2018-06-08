246c5c96558c83bf6237dbc9b1c5a0e3b880c299
Lars Schipper <lars-schipper@hotmail.com>
2018-06-08 11:36:55 +0200

    sensitivity add to contours

--- src/entities/vision/saving.py
+++ src/entities/vision/saving.py
@@ -44,7 +44,7 @@ class Saving(object):
 
             # Append the valid contours to the positions array
             for cnt in range(len(valid_contours)):
-                self.positions = self.helper.append_to_positions(self.positions, valid_contours[cnt])
+                self.positions = self.helper.append_to_positions(self.positions, valid_contours[cnt], 5)
 
             if cv2.waitKey(1) & 0xFF == ord('s'):
                 self.show_input_fields()
