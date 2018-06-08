7471cd1524abbd1068935c66e66969fedc8bfd4f
Lars Schipper <lars-schipper@hotmail.com>
2018-06-08 11:03:16 +0200

    move print pos

--- src/entities/vision/saving.py
+++ src/entities/vision/saving.py
@@ -50,9 +50,9 @@ class Saving(object):
                 self.show_input_fields()
 
             if self.save and 3 < len(self.positions) == self.save_length:
-                self.save_building(mask_cropped, self.positions)
                 for pos in range(len(self.positions)):
                     print(self.positions[pos])
+                self.save_building(mask_cropped, self.positions)
 
             # Show the created image
             cv2.imshow('Spider Cam 3000', mask_cropped)
