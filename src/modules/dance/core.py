5ab6d4187733d1ff40a13966eadef631a1d0bee0
Rendall Schijven <rendall.schijven@hotmail.com>
2018-06-08 14:23:48 +0200

    Pirouette test

--- src/modules/dance/core.py
+++ src/modules/dance/core.py
@@ -13,7 +13,7 @@ def run(name, shared_object, movement):
     while not shared_object.has_to_stop():
         print("Doing calculations and stuff")
 
-        tracks.turn_right(100, 100, 5, 10)
+        tracks.turn_right(100, 100, 0, 10)
 
     # Notify shared object that this thread has been stopped
     shared_object.has_been_stopped()
