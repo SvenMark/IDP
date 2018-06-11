7f93ec03f0241cfd1eb355411135a5a9f3f9dfd0
Lars Schipper <lars-schipper@hotmail.com>
2018-06-11 14:01:36 +0200

    change line

--- src/modules/transport_rebuild/core.py
+++ src/modules/transport_rebuild/core.py
@@ -54,8 +54,7 @@ def run(shared_object):
         else:
             threading.Thread(target=vision.recognize.run).start()
     else:
-        threading.Thread(target=vision.recognize
-                         .run).start()
+        threading.Thread(target=vision.recognize.run).start()
 
 
 run("")
