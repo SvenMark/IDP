bd9c8839b83c270dc4cf0faf4857df28fc344c05
Lars Schipper <lars-schipper@hotmail.com>
2018-06-14 12:00:51 +0200

    new default array

--- src/entities/vision/helpers/json_handler.py
+++ src/entities/vision/helpers/json_handler.py
@@ -15,7 +15,6 @@ class Json_Handler:
                                     Color("orange", [0, 108, 104], [6, 255, 255]),
                                     Color("green", [28, 39, 0], [94, 255, 255]),
                                     Color("red", [167, 116, 89], [180, 255, 255])]
-        self.back_up_building = [Building((0, 0), (0, 0), (0, 0), (0, 0), False, 99)]
 
     def set_color_range(self, color_range):
         saved_file = open(self.file_name_color, "w")
@@ -40,18 +39,19 @@ class Json_Handler:
     def set_save_building(self, positions, building, side):
         current = self.get_save_buildings()
         exist = False
-        for saved_building in current:
-            print(saved_building.number)
-            if saved_building.number == building:
-                exist = True
-                if side == Side.front:
-                    saved_building.front = positions
-                elif side == Side.back:
-                    saved_building.back = positions
-                elif side == Side.left:
-                    saved_building.left = positions
-                elif side == Side.right:
-                    saved_building.right = positions
+        if len(current) > 0:
+            for saved_building in current:
+                print(saved_building.number)
+                if saved_building.number == building:
+                    exist = True
+                    if side == Side.front:
+                        saved_building.front = positions
+                    elif side == Side.back:
+                        saved_building.back = positions
+                    elif side == Side.left:
+                        saved_building.left = positions
+                    elif side == Side.right:
+                        saved_building.right = positions
 
         if not exist:
             new_building = Building(number=building)
@@ -82,7 +82,7 @@ class Json_Handler:
 
             saved_file.close()
         except FileNotFoundError:
-            saved_building = self.back_up_building
+            saved_building = []
 
         return saved_building
 
