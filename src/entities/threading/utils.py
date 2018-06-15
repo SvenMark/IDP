b192e119f6fbc405769aa801ffc95e2f76efdc37
Jildert <jildert.venema@hotmail.com>
2018-06-15 08:28:56 +0200

    comments

--- src/entities/threading/utils.py
+++ src/entities/threading/utils.py
@@ -5,29 +5,29 @@ sys.path.insert(0, '../../../src')
 
 class SharedObject(object):
     """
-
+    Shared object used in threading to notify threads to stop
     """
 
     def __init__(self):
         """
-
+        Constructor for Shared Object class
         """
         self.stop = False
         self.has_stopped = True
 
-    # Get status
     def has_to_stop(self):
         """
-
-        :return:
+        Get status has to stop
+        :return: if Thread has to stop
         """
         return self.stop
 
     # Set status
     def has_been_stopped(self):
         """
-
-        :return:
+        Set status has been stopped to notify the main thread that the current thread
+        has stopped
+        :return: Thread has stopped
         """
         self.has_stopped = True
 
