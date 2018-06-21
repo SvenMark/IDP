a45d3ee8dcf4ef4b17cc1de989c3d003fe159c04
Rendall Schijven <rendall.schijven@hotmail.com>
2018-06-21 17:10:59 +0200

    update sequence

--- src/tests/sequence_tests.py
+++ src/tests/sequence_tests.py
@@ -23,8 +23,9 @@ tracks = Tracks(track_0_pin=13,
 for i in range(5):
     tracks.stop()
     legs.run_sequence(speeds=[150, 150, 150], self_update=True, sequences=[0], sequence=stair)
-    time.sleep(2)
-    tracks.forward(50, 50, 0, 0)
+    time.sleep(1.)
+    tracks.forward(50, 50, 2, 0)
     legs.run_sequence(speeds=[150, 150, 150], self_update=True, sequences=[1], sequence=stair)
+    time.sleep(2)
 
 legs.retract(150)
