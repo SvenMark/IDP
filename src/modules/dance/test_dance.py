5052032e6c9e51dba03ba70e68493e77f3754972
Jildert <jildert.venema@hotmail.com>
2018-06-25 13:11:51 +0200

    numbers

--- src/modules/dance/test_dance.py
+++ src/modules/dance/test_dance.py
@@ -7,15 +7,15 @@ print("[RUN] " + str("test dance"))
 
 update = False
 
-# 0:00.21       intro
-# 0:20.66       next part intro
-# 0:36.04       next part intro
-# 0:51.35       pitch speedup
-# 0:58.17       swing start
-# 1:29.24       swing drop
-# 1:42.91       brake (tape stop)
-# 1:43.21       rasputin original
-# 1:59.43       last hit
+# 1 0:00.21       intro
+# 2 0:20.66       next part intro
+# 3 0:36.04       next part intro
+# 4 0:51.35       pitch speedup
+# 5 0:58.17       swing start
+# 6 1:29.24       swing drop
+# 7 1:42.91       brake (tape stop)
+# 8 1:43.21       rasputin original
+# 9 1:59.43       last hit
 
 
 def routine():
