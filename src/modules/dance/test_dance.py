dde41bdda0fd29d2e00c81bef73622417800e848
Jildert <jildert.venema@hotmail.com>
2018-06-25 13:11:28 +0200

    parts

--- src/modules/dance/test_dance.py
+++ src/modules/dance/test_dance.py
@@ -7,6 +7,16 @@ print("[RUN] " + str("test dance"))
 
 update = False
 
+# 0:00.21       intro
+# 0:20.66       next part intro
+# 0:36.04       next part intro
+# 0:51.35       pitch speedup
+# 0:58.17       swing start
+# 1:29.24       swing drop
+# 1:42.91       brake (tape stop)
+# 1:43.21       rasputin original
+# 1:59.43       last hit
+
 
 def routine():
     global update
