cff6bb53174b8b52862273ab21bf7c1a61a7a1e8
AltijdMelvin <bos12bos@hotmail.com>
2018-06-26 10:51:38 +0200

    Changes

--- src/entities/vision/obstacle_settings.py
+++ src/entities/vision/obstacle_settings.py
@@ -1,15 +1,14 @@
 import sys
-
 sys.path.insert(0, '../../../src')
 
 
-class CupSettings(object):
+class ObstacleSettings(object):
     def __init__(self):
         # Als je een frame verwerkt hebt
         self.update = False
 
-        # als je de beker hebt gedetecteerd
+        # Als je de beker hebt gedetecteerd
         self.cup_detected = False
 
-        # positie van de beker
+        # Positie van de beker
         self.cup_position = 50
--- src/modules/obstacle_course/core.py
+++ src/modules/obstacle_course/core.py
@@ -7,7 +7,26 @@ sys.path.insert(0, '../../../src')
 
 from imutils.video import VideoStream
 from entities.vision.recognize import Recognize
+from entities.vision.obstacle_settings import ObstacleSettings
 from entities.vision.helpers.vision_helper import *
+
+
+def obstacle_course(shared_object):
+    frame = VideoStream(src=0, usePiCamera=True, resolution=(320, 240)).start()
+    time.sleep(0.3)  # startup
+
+    sample = frame.read()
+    height, width = sample.shape
+    print("[INFO] W: " + str(width) + "px , H: " + str(height) + "px")
+
+    while not shared_object.has_to_stop():
+
+        # If q is pressed, break while loop
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    frame.stop()
+    cv2.destroyAllWindows()
 
 
 def run(name, control):
@@ -31,14 +50,7 @@ def run(name, control):
     shared_object.has_been_stopped()
 
 
-def stairdetector(shared_object):
-    frame = VideoStream(src=0, usePiCamera=True, resolution=(320, 240)).start()
-    time.sleep(0.3)  # startup
-
-    sample = frame.read()
-    height, width, channel = sample.shape
-    print("[INFO] w:" + str(width) + ", h: " + str(height))
-
+def stairdetector(frame, width, height):
     vertices = [
         (0, (height / 2) + 28),
         (width, (height / 2 + 28)),
@@ -46,97 +58,75 @@ def stairdetector(shared_object):
         (0, (height / 2) + 48),
     ]
 
-    while not shared_object.has_to_stop():
-        # Get current frame from picamera and make a cropped image with the vertices above with set_region
-        img = frame.read()
-        img_cropped = set_region(img, np.array([vertices], np.int32))
+    # Get current frame from picamera and make a cropped image with the vertices above with set_region
+    img = frame.read()
+    img_cropped = set_region(img, np.array([vertices], np.int32))
 
-        # Add blur to the cropped image
-        blur = cv2.GaussianBlur(img_cropped, (9, 9), 0)
+    # Add blur to the cropped image
+    blur = cv2.GaussianBlur(img_cropped, (9, 9), 0)
 
-        # Generate and set a mask for a range of black (color of the line) to the cropped image
-        hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-        white = Color("Staircolor", [95, 0, 140], [180, 35, 255])
-        mask = cv2.inRange(hsv, white.lower, white.upper)
+    # Generate and set a mask for a range of black (color of the line) to the cropped image
+    hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+    white = Color("Staircolor", [95, 0, 140], [180, 35, 255])
+    mask = cv2.inRange(hsv, white.lower, white.upper)
 
-        # Set line color to blue and clone the image to draw the lines on
-        line_color = (255, 0, 0)
-        img_clone = img.copy()
+    # Set line color to blue and clone the image to draw the lines on
+    line_color = (255, 0, 0)
+    img_clone = img.copy()
 
-        # Set variables and get lines with Houghlines function on the mask of black
-        theta = np.pi / 180
-        threshold = 150
-        min_line_length = 310
-        max_line_gap = 50
+    # Set variables and get lines with Houghlines function on the mask of black
+    theta = np.pi / 180
+    threshold = 150
+    min_line_length = 310
+    max_line_gap = 50
 
-        lines = cv2.HoughLinesP(mask, 2, theta, threshold, np.array([]),
-                                min_line_length, max_line_gap)
+    lines = cv2.HoughLinesP(mask, 2, theta, threshold, np.array([]),
+                            min_line_length, max_line_gap)
 
-        if lines is not None:
-            print("[INFO] Stair Detected!")
-            for line in lines:
-                for x1, y1, x2, y2 in line:
-                    # Make two points with the pixels of the line and draw the line on the cloned image
-                    p1 = Point(x1, y1)
-                    p2 = Point(x2, y2)
-                    cv2.line(img_clone, (p1.x, p1.y), (p2.x, p2.y), line_color, 2)
+    if lines is not None:
+        print("[INFO] Stair Detected!")
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                # Make two points with the pixels of the line and draw the line on the cloned image
+                p1 = Point(x1, y1)
+                p2 = Point(x2, y2)
+                cv2.line(img_clone, (p1.x, p1.y), (p2.x, p2.y), line_color, 2)
 
-        cv2.imshow('camservice-stair', img_clone)
-
-        # If q is pressed, break while loop
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    frame.stop()
-    cv2.destroyAllWindows()
+    cv2.imshow('camservice-stair', img_clone)
 
 
-def detect_cup(shared_object):
+def detect_cup(frame, width, height):
     print("[RUN] Cup Detection")
-    cam = VideoStream(src=0, usePiCamera=False, resolution=(320, 240)).start()
-    time.sleep(0.3)  # startup
+    img = frame.read()
+    img = cv2.GaussianBlur(img, (9, 9), 0)
 
-    sample = cam.read()
-    height, width, channel = sample.shape
-    print("[INFO] W: " + str(width) + "px , H: " + str(height) + "px")
+    # Generate and set a mask for a range of black (color of the line) to the cropped image
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    cupc = Color("Cup Color", [30, 10, 60], [80, 135, 160])
+    mask = cv2.inRange(hsv, cupc.lower, cupc.upper)
+    mask_green = cv2.bitwise_and(img, img, mask=mask)
 
-    while not shared_object.has_to_stop():
-        frame = cam.read()
-        frame = cv2.GaussianBlur(frame, (9, 9), 0)
+    # Get contours and draw them when area of them is 1000 or higher
+    ret, thresh = cv2.threshold(mask, 127, 255, 0)
+    im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        # Generate and set a mask for a range of black (color of the line) to the cropped image
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        cupc = Color("Cup Color", [30, 10, 60], [80, 135, 160])
-        mask = cv2.inRange(hsv, cupc.lower, cupc.upper)
-        mask_green = cv2.bitwise_and(frame, frame, mask=mask)
+    if len(contours) != 0:
+        c = max(contours, key=cv2.contourArea)
+        m = cv2.moments(c)
+        try:
+            cx = int(m["m10"] / m["m00"])
+            cy = int(m["m01"] / m["m00"])
+        except ZeroDivisionError:
+            cx = 0
+            cy = 0
 
-        # Get contours and draw them when area of them is 1000 or higher
-        ret, thresh = cv2.threshold(mask, 127, 255, 0)
-        im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        distanceleft = cx / width * 100
+        distanceright = (width - cx) / width * 100
 
-        if len(contours) != 0:
-            c = max(contours, key=cv2.contourArea)
-            m = cv2.moments(c)
-            try:
-                cx = int(m["m10"] / m["m00"])
-                cy = int(m["m01"] / m["m00"])
-            except ZeroDivisionError:
-                cx = 0
-                cy = 0
+        cv2.drawContours(mask_green, [c], -1, (255, 255, 255), 5)
+        cv2.circle(mask_green, (cx, cy), 7, (255, 255, 255), -1)
 
-            distanceleft = cx / width * 100
-            distanceright = (width - cx) / width * 100
-
-            cv2.drawContours(mask_green, [c], -1, (255, 255, 255), 5)
-            cv2.circle(mask_green, (cx, cy), 7, (255, 255, 255), -1)
-
-        cv2.imshow("Cup center", mask_green)
-
-        if cv2.waitKey(10) == ord('q'):
-            break
-
-    cam.stop()
-    cv2.destroyAllWindows()
+    cv2.imshow("Cup center", mask_green)
 
 
 def detect_bridge():
