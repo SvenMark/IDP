19d86a58141b207664ac56e9ffed5ace37b040e9
kevin <kevinsnijder9@hotmail.com>
2018-06-26 20:00:21 +0200

    Heeft kevin geschreven dit

--- src/modules/dance/core.py
+++ src/modules/dance/core.py
@@ -1,6 +1,7 @@
 import sys
 import time
-
+import RPi.GPIO as GPIO
+from entities.movement.sequences.sequences import *
 sys.path.insert(0, '../../../src')
 
 
@@ -11,17 +12,40 @@ def run(name, control):
     speed_factor = control.speed_factor
     dead_zone = control.dead_zone
 
-    BPM = 150
-    delay = 60 / 150
-
     print("[RUN] " + str(name))
 
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
+    beat_led_pin = 16
+    beat_detect_pin = 20
+    beat_pin = 21
+    sequences = [clap, ballerina, extend_arms, vagedraai, runningman, shakeass,
+                 dabrechts, dablinks, dabrechts, dablinks, dabrechts]
+    GPIO.setup(beat_led_pin, GPIO.OUT)
+    GPIO.setup(beat_detect_pin, GPIO.OUT)
+    GPIO.setup(beat_pin, GPIO.IN)
+    GPIO.output(beat_led_pin, 1)
+    GPIO.output(beat_detect_pin, 1)
+
+    step = 0
+    current = 0
+
     while not shared_object.has_to_stop():
-        print("Doing calculations and stuff")
+        if GPIO.input(beat_pin):
+            movement.legs.move(sequences[current][step][0], sequences[current][step][1],sequences[current][step][2],
+                               sequences[current][step][3], [100, 100, 100], True)
+            step += 1
+            if step >= len(sequences[current]):
+                step = 0
+                current += 1
+                if current >= len(sequences):
+                    shared_object.stop = True
 
-        movement.tracks.turn_right(30, 30, 0.1, 0)
-        time.sleep(delay)
-
+    GPIO.output(beat_led_pin, 0)
+    GPIO.output(beat_detect_pin, 0)
+    GPIO.cleanup(beat_led_pin)
+    GPIO.cleanup(beat_detect_pin)
     # Notify shared object that this thread has been stopped
     print("[STOPPED]" + str(name))
     shared_object.has_been_stopped()
