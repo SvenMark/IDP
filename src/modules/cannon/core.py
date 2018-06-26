aac5e3293aa3cfc8e4b760f8e9007c120131e2cd
Rendall Schijven <rendall.schijven@hotmail.com>
2018-06-26 13:58:13 +0200

    remove kebab

--- src/modules/cannon/core.py
+++ src/modules/cannon/core.py
@@ -104,7 +104,7 @@ def line_detection(shared_object):
 
         # Generate and set a mask for a range of black (color of the line) to the cropped image
         hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-        black = Color("Black", [0, 0, 40], [157, 118, 120])
+        black = Color("Black", [0, 0, 0], [43, 65, 80])
         mask = cv2.inRange(hsv, black.lower, black.upper)
 
         # Checks if the color red is detected and calls function detect_red with the img
