3b86ee9756af20baa1f028e032b9760fc9aeef76
Rendall Schijven <rendall.schijven@hotmail.com>
2018-06-27 12:35:32 +0200

    updt loosen

--- src/entities/movement/grabber.py
+++ src/entities/movement/grabber.py
@@ -42,7 +42,7 @@ class Grabber(object):
         :return: None
         """
         self.grabbing = True
-        positions = [830, 510, 120]  # The servo positions for grabbing
+        positions = [770, 580, 130]  # The servo positions for grabbing
 
         if pick_up_vertical:
             self.servos[0].move(positions[0], speed)
